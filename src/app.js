4e48eaf99ec9020c9464704bd06458975af50071
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-03 02:54:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,27 +6,38 @@ const users   =require("../models/user.js");
 //Router Middlewares
 app.use(express.json());
 
-//Type of query (Hint)
-
-/*
-
-1. / --> this means we need to consider all users
-2. /?name=swa --> Will return count of all the user name that have prefix swa. We will (Swaraj Jain, Swarak agrawal, etc). 
-3. /?name= -->this means we need to consider all users
-
-*/
-
-
-// Complete this Route which will return the count of Number of Prefixmatch for the name in the query/
 
 app.get("/",async function(req,res){
 
-    var count = 0;
+    var name = req.query.name, count = 0;
 
-    //Write you code here
-    //update count variable
+    result = await users.find({});
 
-    res.send(JSON.stringify(count));
+    if(typeof name === 'undefined'){
+        count = result.length;
+        res.send(JSON.stringify(count));
+    }
+    else{
+        //swa
+        name = name.toLowerCase();
+        //[swarja,swarna]        
+        for(var i = 0; i < result.length; i++){
+
+            var len = name.length, match = 1, cur_name = result[i]['name'].toLowerCase();
+            if(cur_name.length >= len){
+                
+                for(var j=0;j<len; j++){
+                    if(cur_name[j] != name[j]){
+                        match = 0;
+                        break;
+                    }
+                }
+            }
+            count += match;
+        }
+        
+        res.send(JSON.stringify(count));
+    }
 
 });
 
